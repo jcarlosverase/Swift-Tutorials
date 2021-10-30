298bcc1ea99e29bd603f1e69c11ca00523de5cae
Juan Carlos <jcarlosverase@gmail.com>
2021-10-29 22:03:26 -0500

    Add changes

---
+++ ProductStore/ProductStore/ProductStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductStoreApp.swift
+//  ProductStore
+//
+//  Created by Juan Alberto Carlos Vera on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct ProductStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
